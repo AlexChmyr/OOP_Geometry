17b91e24d3a33a9704b43ae014189906034f380c
Alex Chmyr <chmyr.aleksandr@mail.ru>
2018-03-22 21:48:56 +0200

    add mockup

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    Figure triangle = new Rectangle();
+	    Figure rectangle = new Rectangle();
+        rectangle.addPoint(new ConcretePoint(1,1));
+        rectangle.addPoint(new ConcretePoint(2, 1));
+        rectangle.addPoint(new ConcretePoint(2,2));
+        rectangle.addPoint(new ConcretePoint(1,2));
+
+        rectangle.print();
+
+        Figure triangle = new Triangle();
         triangle.addPoint(new ConcretePoint(1,1));
-        triangle.addPoint(new ConcretePoint(2, 1));
-        triangle.addPoint(new ConcretePoint(2,2));
-        triangle.addPoint(new ConcretePoint(1,2));
+        triangle.addPoint(new ConcretePoint(5, 2));
+        triangle.addPoint(new ConcretePoint(3,3));
 
         triangle.print();
-
     }
 }
